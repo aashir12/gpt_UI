4fc9c65d38b4e789629a7eab1669a69b70b6d854
unknown <aashirnawaz5@gmail.com>
2024-07-14 22:43:47 +0500

    chatgpt ui

---
+++ src/App.jsx
@@ -0,0 +1,276 @@
+import {
+  useState,
+  useEffect,
+  useRef,
+  useCallback,
+  useLayoutEffect,
+} from "react";
+import { BiPlus, BiUser, BiSend, BiSolidUserCircle } from "react-icons/bi";
+import { MdOutlineArrowLeft, MdOutlineArrowRight } from "react-icons/md";
+
+function App() {
+  const [text, setText] = useState("");
+  const [message, setMessage] = useState(null);
+  const [previousChats, setPreviousChats] = useState([]);
+  const [localChats, setLocalChats] = useState([]);
+  const [currentTitle, setCurrentTitle] = useState(null);
+  const [isShowSidebar, setIsShowSidebar] = useState(false);
+  const scrollToLastItem = useRef(null);
+
+  const createNewChat = () => {
+    setMessage(null);
+    setText("");
+    setCurrentTitle(null);
+  };
+
+  const backToHistoryPrompt = (uniqueTitle) => {
+    setCurrentTitle(uniqueTitle);
+    setMessage(null);
+    setText("");
+  };
+
+  const toggleSidebar = useCallback(() => {
+    setIsShowSidebar((prev) => !prev);
+  }, []);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (!text) return;
+
+    const newChat = {
+      title: currentTitle || text,
+      role: "user",
+      content: text,
+    };
+
+    const responseMessage = {
+      title: currentTitle || text,
+      role: "ChatGPT",
+      content: `thanks for creating a prompt we will get back to you"`,
+    };
+
+    setPreviousChats((prevChats) => [...prevChats, newChat, responseMessage]);
+    setLocalChats((prevChats) => [...prevChats, newChat, responseMessage]);
+
+    setMessage(responseMessage);
+
+    setTimeout(() => {
+      scrollToLastItem.current?.lastElementChild?.scrollIntoView({
+        behavior: "smooth",
+      });
+    }, 1);
+    setTimeout(() => {
+      setText("");
+    }, 2);
+  };
+
+  useLayoutEffect(() => {
+    const handleResize = () => {
+      setIsShowSidebar(window.innerWidth <= 640);
+    };
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    const storedChats = localStorage.getItem("previousChats");
+
+    if (storedChats) {
+      setLocalChats(JSON.parse(storedChats));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (!currentTitle && text && message) {
+      setCurrentTitle(text);
+    }
+
+    if (currentTitle && text && message) {
+      const newChat = {
+        title: currentTitle,
+        role: "user",
+        content: text,
+      };
+
+      const responseMessage = {
+        title: currentTitle,
+        role: message.role,
+        content: message.content,
+      };
+
+      const updatedChats = [...localChats, newChat, responseMessage];
+      localStorage.setItem("previousChats", JSON.stringify(updatedChats));
+    }
+  }, [message, currentTitle]);
+
+  const currentChat = (localChats || previousChats).filter(
+    (prevChat) => prevChat.title === currentTitle
+  );
+
+  const uniqueTitles = Array.from(
+    new Set(previousChats.map((prevChat) => prevChat.title).reverse())
+  );
+
+  const localUniqueTitles = Array.from(
+    new Set(localChats.map((prevChat) => prevChat.title).reverse())
+  ).filter((title) => !uniqueTitles.includes(title));
+
+  return (
+    <>
+      <div className="container">
+        <section className={`sidebar ${isShowSidebar ? "open" : ""}`}>
+          <div className="sidebar-header" onClick={createNewChat} role="button">
+            <BiPlus size={20} />
+            <button>New Chat</button>
+          </div>
+          <div className="sidebar-history">
+            {uniqueTitles.length > 0 && previousChats.length !== 0 && (
+              <>
+                <p>Ongoing</p>
+                <ul>
+                  {uniqueTitles?.map((uniqueTitle, idx) => {
+                    const listItems = document.querySelectorAll("li");
+
+                    listItems.forEach((item) => {
+                      if (item.scrollWidth > item.clientWidth) {
+                        item.classList.add("li-overflow-shadow");
+                      }
+                    });
+
+                    return (
+                      <li
+                        key={idx}
+                        onClick={() => backToHistoryPrompt(uniqueTitle)}
+                      >
+                        {uniqueTitle}
+                      </li>
+                    );
+                  })}
+                </ul>
+              </>
+            )}
+            {localUniqueTitles.length > 0 && localChats.length !== 0 && (
+              <>
+                <p>Previous</p>
+                <ul>
+                  {localUniqueTitles?.map((uniqueTitle, idx) => {
+                    const listItems = document.querySelectorAll("li");
+
+                    listItems.forEach((item) => {
+                      if (item.scrollWidth > item.clientWidth) {
+                        item.classList.add("li-overflow-shadow");
+                      }
+                    });
+
+                    return (
+                      <li
+                        key={idx}
+                        onClick={() => backToHistoryPrompt(uniqueTitle)}
+                      >
+                        {uniqueTitle}
+                      </li>
+                    );
+                  })}
+                </ul>
+              </>
+            )}
+          </div>
+          <div className="sidebar-info">
+            <div className="sidebar-info-upgrade">
+              <BiUser size={20} />
+              <p>Upgrade plan</p>
+            </div>
+            <div className="sidebar-info-user">
+              <BiSolidUserCircle size={20} />
+              <p>User</p>
+            </div>
+          </div>
+        </section>
+
+        <section className="main">
+          {!currentTitle && (
+            <div className="empty-chat-container">
+              <img
+                src="images/chatgpt-logo.svg"
+                width={45}
+                height={45}
+                alt="ChatGPT"
+              />
+              <h1>Chat GPT Clone</h1>
+              <h3>How can I help you today?</h3>
+            </div>
+          )}
+
+          {isShowSidebar ? (
+            <MdOutlineArrowRight
+              className="burger"
+              size={28.8}
+              onClick={toggleSidebar}
+            />
+          ) : (
+            <MdOutlineArrowLeft
+              className="burger"
+              size={28.8}
+              onClick={toggleSidebar}
+            />
+          )}
+          <div className="main-header">
+            <ul>
+              {currentChat?.map((chatMsg, idx) => {
+                const isUser = chatMsg.role === "user";
+
+                return (
+                  <li key={idx} ref={scrollToLastItem}>
+                    {isUser ? (
+                      <div>
+                        <BiSolidUserCircle size={28.8} />
+                      </div>
+                    ) : (
+                      <img src="images/chatgpt-logo.svg" alt="ChatGPT" />
+                    )}
+                    {isUser ? (
+                      <div>
+                        <p className="role-title">You</p>
+                        <p>{chatMsg.content}</p>
+                      </div>
+                    ) : (
+                      <div>
+                        <p className="role-title">ChatGPT</p>
+                        <p>{chatMsg.content}</p>
+                      </div>
+                    )}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+          <div className="main-bottom">
+            <form className="form-container" onSubmit={submitHandler}>
+              <input
+                type="text"
+                placeholder="Send a message."
+                spellCheck="false"
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+              />
+              <button type="submit">
+                <BiSend size={20} />
+              </button>
+            </form>
+            <p>
+              ChatGPT can make mistakes. Consider checking important
+              information.
+            </p>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+}
+
+export default App;
